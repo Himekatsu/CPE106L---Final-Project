b5b67606fe355bf69793bb77a3fc691522514981
Phantom5468 <jagpelagio12345@gmail.com>
2025-06-02 21:37:00 +0800

    update

--- Program test for commit.py
+++ Program test for commit.py
@@ -5,5 +5,5 @@ i = int(input("Enter a Current: "))
 p = v * i
 r = v / i
 print("Power is: ", p, "Watts")
-print("Resistance is: ", r, "Ohms")
+print("Resistance is: ", round(r,2), "Ohms")
 print("Done")
