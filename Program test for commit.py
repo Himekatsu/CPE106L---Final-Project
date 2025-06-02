fb05dd236aeeaf583ab40886bf5bf53e2c4ba919
Phantom5468 <jagpelagio12345@gmail.com>
2025-06-02 21:36:00 +0800

    Testing

---
+++ Program test for commit.py
@@ -0,0 +1,9 @@
+print("Program test")
+
+v = int(input("Enter a Voltage: "))
+i = int(input("Enter a Current: "))
+p = v * i
+r = v / i
+print("Power is: ", p, "Watts")
+print("Resistance is: ", r, "Ohms")
+print("Done")
