d3ed41df608856fc914c3e782ed365f213da6310
Phantom5468 <jagpelagio12345@gmail.com>
2025-06-07 13:26:09 +0800

    New commit

--- Lab 4/codes/Lab4-unittest.py
+++ Lab 4/codes/Lab4-unittest.py
@@ -1,8 +1,9 @@
 import unittest
+import oxo_gui_complete
 
 class gameTest(unittest.TestCase):
-    def testSample(self):
-        self.assertEqual(1+1,2)
+    def testSample(oxo_gui_complete):
+        oxo_gui_complete.assertEqual(1+1,2)
 
 if __name__ == '__main__':
     unittest.main()
