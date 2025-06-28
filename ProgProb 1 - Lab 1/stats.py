5fdfa4c5093e62e746619fd1ae765e0f06c806c8
Phantom5468 <jagpelagio12345@gmail.com>
2025-06-28 12:00:16 +0800

    Copilot assistance with mean programming

--- ProgProb 1 - Lab 1/stats.py
+++ ProgProb 1 - Lab 1/stats.py
@@ -1,10 +1,17 @@
+def mean(numSet,A):
+    total = sum(numSet)
+    cout = len(numSet)
+    ave = total / cout
+    print("The mean is", round(total, 2))
 
 def main():
+    numSet = []
     A = int(input("How many set of numbers will you input: "))
     for i in range (0,A,1):
         x = int(input("Enter a number: "))
-        numSet = []
         numSet.append(x)
+    mean
+    
 
 if __name__ == "__main__":
     main()
