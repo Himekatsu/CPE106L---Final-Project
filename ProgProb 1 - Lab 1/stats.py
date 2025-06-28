c822aa0c84de7c8e1a916114ed5ba6b268d09b0d
Phantom5468 <jagpelagio12345@gmail.com>
2025-06-28 11:37:15 +0800

    Setting up def main

---
+++ ProgProb 1 - Lab 1/stats.py
@@ -0,0 +1,10 @@
+
+def main():
+    A = int(input("How many set of numbers will you input: "))
+    for i in range (0,A,1):
+        x = int(input("Enter a number: "))
+        numSet = []
+        numSet.append(x)
+
+if __name__ == "__main__":
+    main()
