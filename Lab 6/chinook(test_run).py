bcf0374e9c1db033150b0cc680464d54c01c64f4
Phantom5468 <jagpelagio12345@gmail.com>
2025-07-06 20:40:27 +0800

    Testing complete and working

--- Lab 6/chinook(test_run).py
+++ Lab 6/chinook(test_run).py
@@ -2,11 +2,18 @@ from pymongo import MongoClient
 import pprint
 import re
 
-# client = MongoClient(host='localhost', port=27017)
-client = MongoClient('mongodb://localhost:27017/')
+# client = MongoClient(host="localhost", port=27017)
+client = MongoClient("mongodb://localhost:27017/")
+
+# Get reference to 'chinook' database
 db = client["chinook"]
-collection = db["customers"]
-doc1 = collection.find_one()
+
+# Get a reference to the 'customers' collection
+customers_collection = db["customers"]
+# print(customers_collection)
+
+#print first document
+doc1 = customers_collection.find_one()
 print(doc1)
 
 client.close()
